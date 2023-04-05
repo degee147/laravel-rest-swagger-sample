5c0aa7953eab93c539059270ae01abab5e49ff6e
Ken Waribo <degee147@gmail.com>
2023-04-05 18:36:23 +0100

    added swagger api documentation

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -13,15 +13,44 @@ class ArticleController extends Controller
 {
     use Ultainfinity;
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
+     * @OA\Get(
+     *     path="/articles",
+     *     summary="Get Articles",
+     *     description="Returns a paginated list of articles",
+     *     @OA\Response(
+     *         response=200,
+     *         description="Everything OK"
+     *     )
+     * )
      */
     public function index()
     {
         $articles = Article::orderBy('id', 'desc')->paginate(10);
         return response()->json($articles, 200);
     }
+
+    /**
+     * @OA\Get(
+     *     path="/articles/{id}",
+     *     summary="Get a single Article",
+     *     description="Returns a single article",
+     *    @OA\Parameter(
+     *         name="article_id",
+     *         in="path",
+     *         description="ID of article to show",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="integer",
+     *             format="int64",
+     *             example=1
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Everything OK"
+     *     )
+     * )
+     */
     public function show($article_id)
     {
         $article = Article::find($article_id);
@@ -30,6 +59,28 @@ class ArticleController extends Controller
         }
         return $this->AppResponse('failed', 'Article not found', 404);
     }
+
+     /**
+     * @OA\Get(
+     *     path="/articles/{id}/like",
+     *     summary="Like a single Article",
+     *      @OA\Parameter(
+     *         name="article_id",
+     *         in="path",
+     *         description="ID of article to like",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="integer",
+     *             format="int64",
+     *             example=1
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Everything OK"
+     *     )
+     * )
+     */
     public function like($article_id)
     {
         $article = Article::find($article_id);
@@ -41,6 +92,28 @@ class ArticleController extends Controller
         }
         return $this->AppResponse('failed', 'Invalid article', 404);
     }
+
+      /**
+     * @OA\Get(
+     *     path="/articles/{id}/view",
+     *     summary="Add view count to a single Article",
+     *      @OA\Parameter(
+     *         name="article_id",
+     *         in="path",
+     *         description="ID of article to add view count",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="integer",
+     *             format="int64",
+     *             example=1
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Everything OK"
+     *     )
+     * )
+     */
     public function view($article_id)
     {
         $article = Article::find($article_id);
@@ -54,7 +127,37 @@ class ArticleController extends Controller
     }
 
 
-
+    /**
+     * @OA\Post(
+     *     path="articles/{id}/comment",
+     *     summary="Add a comment",
+     *     operationId="addcomment",
+     *     description="Send params in form-data. Add Accept:application/json in header",
+     *     operationId="comment",
+     *     @OA\Response(
+     *         response=200,
+     *         description="successful operation",
+     *     ),
+     *     @OA\Parameter(
+     *         name="subject",
+     *         in="query",
+     *         description="subject",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="string"
+     *         )
+     *     ),
+     *    @OA\Parameter(
+     *         name="body",
+     *         in="query",
+     *         description="body",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="string"
+     *         )
+     *     ),
+     * )
+     */
     public function comment(CommentPostRequest $request, $article_id)
     {
         $article = Article::find($article_id);
